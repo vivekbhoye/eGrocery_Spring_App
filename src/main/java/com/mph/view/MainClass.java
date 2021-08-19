6c05eb2ebd9c512a5703a9439bf8e44a30c2d564
vivekbhoye <vivekrbhoye@gmail.com>
2021-08-19 23:08:42 +0530

    View file for testing databse added by vivek

---
+++ src/main/java/com/mph/view/MainClass.java
@@ -0,0 +1,15 @@
+package com.mph.view;
+
+import com.mph.controller.CustomerController;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		CustomerController cc = new CustomerController();
+		cc.addCustomer();
+		
+
+	}
+
+}
