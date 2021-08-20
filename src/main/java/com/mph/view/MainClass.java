1757b0a4312de8b7dce3303de021d16f72ed46ed
SundarRajB <rsundar925@gmail.com>
2021-08-20 12:03:56 +0530

    DAO layer added by Sundar

--- src/main/java/com/mph/view/MainClass.java
+++ src/main/java/com/mph/view/MainClass.java
@@ -1,15 +1,37 @@
 package com.mph.view;
 
-import com.mph.controller.CustomerController;
+import java.util.List;
+import java.util.Scanner;
+
+import com.mph.egrocery.controller.userController;
+import com.mph.egrocery.dao.ProductDao;
+import com.mph.egrocery.dao.ProductDaoImpl;
+
+import  com.mph.egrocery.entity.*;
 
 public class MainClass {
+    public static void main(String[] args) {
+    	
+    	Products product=new Products();
+    	int productId;
+    	Scanner sc = new Scanner(System.in);
+    	
+    	
+    	
+    	
+    	ProductDao dao=new ProductDaoImpl();
 
-	public static void main(String[] args) {
-		
-		CustomerController cc = new CustomerController();
-		cc.addCustomer();
-		
+    	//dao.deleteProduct(productId);
 
-	}
+    	dao.addProduct(product);
+    	
+    	  System.out.println("Enter Product id");
+    	  productId=sc.nextInt();
+    	
+    	dao.getProductById(productId);
 
+    	//dao.updateProduct(product);
+
+    	//dao.getAllProducts();
+    }
 }
